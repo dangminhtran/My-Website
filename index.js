823638bad7cefc9863659fd02fef8711c99e109a
Dang Tran <trmdang@my.yorku.ca>
2023-06-01 09:26:45 -0400

    Fix animation

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function showContactSection() {
-    document.getElementById("profile").classList.add("hidden");
-    setTimeout(function() {
-      document.getElementById("profile").style.display = "none";
-      document.getElementById("contact").style.display = "block";
-      setTimeout(function() {
-        document.getElementById("contact").classList.remove("hidden");
-      }, 20);
-    }, 500);
-  }
+  document.getElementById("profile").style.opacity = "0";
+  setTimeout(function () {
+    document.getElementById("profile").style.display = "none";
+    document.getElementById("contact").style.display = "block";
+    setTimeout(function () {
+      document.getElementById("contact").style.opacity = "1";
+    }, 20);
+  }, 500);
+}
 
-  function showProfileSection() {
-    document.getElementById("contact").classList.add("hidden");
-    setTimeout(function() {
-      document.getElementById("contact").style.display = "none";
-      document.getElementById("profile").style.display = "block";
-      setTimeout(function() {
-        document.getElementById("profile").classList.remove("hidden");
-      }, 20);
-    }, 500);
-  }
+function showProfileSection() {
+  document.getElementById("contact").style.opacity = "0";
+  setTimeout(function () {
+    document.getElementById("contact").style.display = "none";
+    document.getElementById("profile").style.display = "block";
+    setTimeout(function () {
+      document.getElementById("profile").style.opacity = "1";
+    }, 20);
+  }, 500);
+}
